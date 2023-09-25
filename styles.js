09bf18f982ab66fc33a411752354262843e5ccb5
janani-gopi <jananirave19@gmail.com>
2023-09-25 18:17:31 +0530

    refactor:created separate file for styles:styles.js:completed

---
+++ styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    contentContainer: {
+      flex: 1,
+      alignItems: "center",
+    },
+    input: {
+      padding: 10,
+      borderRadius: 25,
+      paddingHorizontal: 10,
+      backgroundColor: "#e5e5e5",
+      marginBottom: 10,
+      margin: 20,
+    },
+    modalopenertext: {
+      padding: 30,
+      color: "#ffc30b",
+    },
+    text:{
+      color: "#ffc30b",
+      textAlign: "center",
+      fontSize: 20,
+    },
+    button:{
+      borderWidth: 2,
+      padding:10,
+      width: 320,
+      borderRadius: 15,
+      borderColor: "#ffc30b",
+    },
+    flexcontainer:{
+      display: "flex",
+      flexDirection: "column",
+      rowGap: 10,
+      alignItems: "center",
+    }
+  });
+   
